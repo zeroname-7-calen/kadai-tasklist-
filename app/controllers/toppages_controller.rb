83b1d91fad6066c523069d10f4cb15766304ef90
zero_name.7_calen <zero.7.calen@gmail.com>
2019-03-02 13:13:00 +0000

    post再修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @task = current_user.tasks.build #form_for用
-      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page]).per(10)
     end
   end
 end
